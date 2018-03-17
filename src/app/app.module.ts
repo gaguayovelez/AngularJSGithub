658b82038810ab157adc7ac4348123a2505589fd
Gonzalo Aguayo <gonzalo.aguayo@jalasoft.com>
2018-03-16 22:54:14 -0400

    Adding users component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule, NgbModule.forRoot()
